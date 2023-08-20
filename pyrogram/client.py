e4a9348d0a5adb3fea36fe085338f02c14cc017b
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-20 16:25:04 +0700

    Update client.py

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -180,7 +180,7 @@ class Client(Methods):
     """
 
     APP_VERSION = f"Pyrogram {__version__}"
-    DEVICE_MODEL = "PyroKar-Ubot"
+    DEVICE_MODEL = "Pyro Project"
     SYSTEM_VERSION = f"{platform.system()} {platform.release()}"
 
     LANG_CODE = "en"
